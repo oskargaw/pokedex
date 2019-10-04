2d2804f1fdea1d94c990cbb270470df45a4830ff
Oskar Gawlak <oskar.gawlak@gmail.com>
2019-10-04 11:17:42 +0200

    Added particles.js library, applied the background using it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
-import "./App.css";
+
+import Particles from "react-particles-js";
+
+import "./App.scss";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 function App() {
-  return <div className="App">HELLO WORLD!</div>;
+  return (
+    <div className="container">
+      <Particles className="particles" params={particlesOptions} />
+    </div>
+  );
 }
 
 export default App;
